4ab2b2fc261d62c183b4ebaf141ea286b77b16d7
zooo <imzooo@163.com>
2016-07-09 13:51:59 +0800

    implement crack

--- encryption_source.py
+++ encryption_source.py
@@ -8,50 +8,92 @@ key = "dsf"
 msg = "goodgirl"#如何处理空格
 print("msg:%s"%(msg))
 
-#生成不包括key的字母字表
-alpha = "abcdefghijklmnopqrstuvwxyz"
-encryt_table = [x for x in alpha if x not in key ]
-
-randmsg = ""
-pos=0
-msg_len = len(msg)
-
 #encryption
 #step 1: mix
-while True:
-    for c in key:
-        #disturb_char_count = random.randint(0, 3)#插入3个随机字符
-        #for i in range(disturb_char_count):
-            #列表中随机选3个
-        randmsg += ''.join( random.sample(encryt_table, random.randint(0, 3)))
-        randmsg += msg[pos]
-        randmsg += c
-        pos += 1
+def encrypt(msg):
+    #生成不包括key的字母字表
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    encryt_table = [x for x in alpha if x not in key ]
+
+    randmsg = ""
+    pos=0
+    msg_len = len(msg)
+    while True:
+        for c in key:
+            #disturb_char_count = random.randint(0, 3)#插入3个随机字符
+            #for i in range(disturb_char_count):
+                #列表中随机选3个
+            randmsg += ''.join( random.sample(encryt_table, random.randint(0, 3)))
+            randmsg += c
+            randmsg += msg[pos]
+            pos += 1
+            if pos == msg_len:
+                break
         if pos == msg_len:
             break
-    if pos == msg_len:
-        break
-print("randmsg:%s"%(randmsg))
+    print("randmsg:%s"%(randmsg))
 
-#step 2: reflect morse code
-alpha = "abcdefghijklmnopqrstuvwxyz"
-pot = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
-       "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
-       "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
-       "-.--", "--.."]
+    #step 2: reflect morse code
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    pot = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+           "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+           "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+           "-.--", "--.."]
 
-#generate morse dictionary 
-morse_table = {}
-for c in alpha:
-    morse_table[c] = pot[ alpha.index(c)]
+    #generate morse dictionary 
+    morse_table = {}
+    for c in alpha:
+        morse_table[c] = pot[ alpha.index(c)]
 
-morse_code = ""
-for c in randmsg:
-    morse_code += morse_table[c]
-    morse_code += ' '
-print("morse_code:%s"%(morse_code))
+    morse_code = ""
+    for c in randmsg:
+        morse_code += morse_table[c]
+        morse_code += ' '
+    print("morse_code:%s"%(morse_code))
+    return morse_code
 
 #crack
+def crack( morse_code, key="dsf" ):
+    #demorse code
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    pot = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+           "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+           "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+           "-.--", "--.."]
+    #generate morse dictionary
+    morse_table_reverse = {}
+    for c in pot:
+        morse_table_reverse[c] = alpha[ pot.index(c)]
 
+    randmsg = ""
+    temp = ""
+    for c in morse_code:
+        #按空格合并
+        if c != ' ':
+            temp += c
+            continue
+        else:
+            randmsg += morse_table_reverse[temp]
+            temp = ""
+        
+    print("randmsg:%s"%(randmsg))
+
+    #acquire msg
+    msg = ""
+    index = 0
+    randmsg_len = len(randmsg)
+    while True:
+        if randmsg[index] in key:
+            #逻辑：找到一个后从下个开始防止 ddx 的情况
+            msg += randmsg[index+1]#index是从头索引
+            index += 1
+        index += 1
+        if index >= randmsg_len:
+            break
+    print("msg:%s"%(msg))    
+
+morse_code = encrypt( msg )
+
+crack( morse_code )   
 
 #print( random.randint(0, 3) )
