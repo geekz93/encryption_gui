eacc5ede7566a56bbee1f1824e39f3b0000de3ad
zooo <imzooo@163.com>
2016-08-02 19:59:44 +0800

    1.添加点击左键功能 2.添加图片

--- encryption_gui.py
+++ encryption_gui.py
@@ -3,11 +3,16 @@ from encryption_source import *
 
 root = Tk()
 #pack(padx, pady) 设置与边框的距离，单位（像素）
-
+root.title("msgEncrypter")
+root.resizable(False, False)
 class Var:
     msg = ""
     cipher = ""
 
+#剪贴板操作
+#root.clipboard_append()
+#root.clipboard_clear()
+#root.clipboard_get()
 
 #初始化拖拽
 #sb = Scrollbar(root)
@@ -28,6 +33,7 @@ bth = 1#30ppi
 tShowMsg = Text(root, width=txtw, height=txth)
 tShowMsg.grid(row=0, column=0, rowspan=2, padx=10, pady=10)
 #sb.config(command=tShowMsg.yview)
+tShowMsg.insert(INSERT, "显示框")
 
 
 #输入框，只能输入一行
@@ -36,13 +42,18 @@ tShowMsg.grid(row=0, column=0, rowspan=2, padx=10, pady=10)
 #使用文本框进行输入
 eInputMsg = Text( root, width=txtw, height=txth )
 eInputMsg.grid( row=2, column=0, rowspan=2, padx=10, pady=10 )
+eInputMsg.insert(INSERT, "输入框")
 
-#索引行和列
-#def show():
-    #删除文本框的内容
-#    tShowMsg.delete(1.0, END)#1.0表示第1行0列，END表示最后一行最后一列？
-    #tShowMsg.insert(INSERT, eInputMsg.get() )
-#    tShowMsg.insert(INSERT, eInputMsg.get(1.0, END))
+def TipClear(event):
+    if event.widget.get(1.0, END) in ("输入框\n", "显示框\n"):#get会自动插入换行符
+        event.widget.delete(1.0, END)
+
+eInputMsg.bind("<Button-1>", TipClear)
+tShowMsg.bind("<Button-1>", TipClear)
+
+def RightPress(event):
+    pass
+eInputMsg.bind("<ButtonRelease-3>", RightPress)
 
 def encrypt_gui():
     tShowMsg.delete(1.0, END)
@@ -51,7 +62,7 @@ def encrypt_gui():
     tShowMsg.insert(INSERT, Var.cipher)
 
 bEncryption = Button( root, text="加密", command=encrypt_gui, width=btw, height=bth)
-bEncryption.grid( row=0, column=1, padx=5, pady=10, sticky=N)
+bEncryption.grid( row=0, column=1, padx=10, pady=10, sticky=N)
 
 def crack_gui():
     tShowMsg.delete(1.0, END)
@@ -71,7 +82,22 @@ Button(root, text="保存", command=sav, width=btw, height=bth).place(x=379, y=1
 #退出
 Button(root, text="退出", command=root.quit, width=btw, height=bth).place(x=379, y=160)
 
+#添加图片
+photo = PhotoImage(file="fang.gif")
+imgLabel = Label(root, image=photo)
+imgLabel.place(x=379, y=210)
 
 
+############
+#窗口居中
+root.update()                       # update window ,must do
+curWidth = root.winfo_reqwidth()    # get current width
+curHeight = root.winfo_height()     # get current height
+scnWidth,scnHeight = root.maxsize() # get screen width and height
+# now generate configuration information
+tmpcnf = '%dx%d+%d+%d'%(curWidth,curHeight,
+(scnWidth-curWidth)/2,(scnHeight-curHeight)/2)
+root.geometry(tmpcnf)
+###########
 mainloop()
 
